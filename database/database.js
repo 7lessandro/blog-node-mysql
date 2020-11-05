f0b20874b88115e9d0ddb6d694b38a24d42ecb02
ascenciodev <alessandroascencio@protonmail.com>
2020-11-05 12:30:58 -0300

    tinymce and new article

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const connection = new Sequelize('aleblog', 'root', '123456', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 })
 
 module.exports = connection;
