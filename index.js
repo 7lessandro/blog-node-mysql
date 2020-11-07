faa97e9eb04e73669eee2fbb87e381e6b5831823
ascenciodev <alessandroascencio@protonmail.com>
2020-11-07 00:43:54 -0300

    menu dropdown categories

--- index.js
+++ index.js
@@ -43,7 +43,9 @@ app.get("/", (req, res) => {
             ['id', 'DESC']
         ]
     }).then(articles => {
-        res.render('index', {articles:articles})
+        Category.findAll().then(categories => {
+            res.render('index', {articles:articles, categories:categories})
+        })
     })
 })
 
@@ -54,7 +56,9 @@ app.get("/:slug", (req, res) => {
         where: {slug: slug}
     }).then(article => {
         if(article != undefined) {
-            res.render('article',  {article:article})
+            Category.findAll().then(categories => {
+                res.render('article', {categories:categories, article:article})
+            })
         } else {
             res.redirect('/')
         }
