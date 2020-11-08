2f38148ef4f69a38094ec8e20093eff92d4d02f4
ascenciodev <alessandroascencio@protonmail.com>
2020-11-07 23:16:21 -0300

    menu done

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,85 +1,91 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const slugify = require('slugify');
+const slugify = require("slugify");
 
 //Model Category
-const Category = require('./Category');
+const Category = require("./Category");
 
 //Router
-router.get('/admin/categories/new', (req, res) => {
-    res.render('admin/categories/new')
+router.get("/admin/categories/new", (req, res) => {
+  res.render("admin/categories/new");
 });
 
-router.post('/categories/save', (req, res) => {
-    var title = req.body.title;
+router.post("/categories/save", (req, res) => {
+  var title = req.body.title;
 
-    if(title != undefined) {
-        Category.create({
-            title: title,
-            slug: slugify(title)
-        }).then(() => {
-            res.redirect('/admin/categories/')
-        })
-
-    } else {
-        res.redirect('/admin/categories/new')
-    }
-});
-
-router.get('/admin/categories', (req, res) => {
-    Category.findAll().then(categories => {
-        res.render('admin/categories/index', {categories: categories})
-    })
-});
-
-router.post('/categories/delete', (req, res) => {
-    var id = req.body.id;
-    if(id != undefined) {
-        if(!isNaN(id)) { //is a number?
-
-            Category.destroy({
-                where: {id: id}
-            }).then(() => {
-                res.redirect('/admin/categories')
-            })
-
-        } else { //if is not a number
-            res.redirect('/admin/categories')
-        }
-    } else { //if is null
-        res.redirect('/admin/categories')
-    }
-});
-
-router.get('/admin/categories/edit/:id', (req, res) => {
-    var id = req.params.id;
-
-    if(isNaN(id)) {
-        res.redirect('/admin/categories')
-    }
-
-    Category.findByPk(id).then((category) => {
-        if(category != undefined) {
-            res.render('admin/categories/edit', {category: category})
-        } else {
-            res.redirect('/admin/categories')
-        }
-    }).catch( error => {
-        res.redirect('/admin/categories')
-    })
-});
-
-router.post('/categories/update', (req, res) => {
-    var id = req.body.id;
-    var title = req.body.title;
-
-    Category.update({title: title, slug: slugify(title)}, {
-        where: {
-            id: id
-        }
+  if (title != undefined) {
+    Category.create({
+      title: title,
+      slug: slugify(title),
     }).then(() => {
-    res.redirect('/admin/categories')
+      res.redirect("/admin/categories/");
+    });
+  } else {
+    res.redirect("/admin/categories/new");
+  }
+});
+
+router.get("/admin/categories", (req, res) => {
+  Category.findAll().then((categories) => {
+    res.render("admin/categories/index", { categories: categories });
+  });
+});
+
+router.post("/categories/delete", (req, res) => {
+  var id = req.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      //is a number?
+
+      Category.destroy({
+        where: { id: id },
+      }).then(() => {
+        res.redirect("/admin/categories");
+      });
+    } else {
+      //if is not a number
+      res.redirect("/admin/categories");
+    }
+  } else {
+    //if is null
+    res.redirect("/admin/categories");
+  }
+});
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+  var id = req.params.id;
+
+  if (isNaN(id)) {
+    res.redirect("/admin/categories");
+  }
+
+  Category.findByPk(id)
+    .then((category) => {
+      if (category != undefined) {
+        res.render("admin/categories/edit", { category: category });
+      } else {
+        res.redirect("/admin/categories");
+      }
     })
+    .catch((error) => {
+      res.redirect("/admin/categories");
+    });
+});
+
+router.post("/categories/update", (req, res) => {
+  var id = req.body.id;
+  var title = req.body.title;
+
+  Category.update(
+    { title: title, slug: slugify(title) },
+    {
+      where: {
+        id: id,
+      },
+    }
+  ).then(() => {
+    res.redirect("/admin/categories");
+  });
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,73 +1,96 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const connection = require('./database/database');
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
 
 //Controllers
-const categoriesController = require('./categories/CategoriesController');
-const articlesController = require('./articles/ArticlesController');
+const categoriesController = require("./categories/CategoriesController");
+const articlesController = require("./articles/ArticlesController");
 
 //Models
-const Article = require('./articles/Article')
-const Category = require('./categories/Category')
+const Article = require("./articles/Article");
+const Category = require("./categories/Category");
 
 //View Engine
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 //Body-Parser
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Static
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 //Database
 connection
-    .authenticate()
-    .then(() => {
-        console.log('Conexão realizada c/ sucesso.')
-    }).catch((error) => {
-        console.log(error)
-    })
+  .authenticate()
+  .then(() => {
+    console.log("Conexão realizada c/ sucesso.");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 //Rotes & Controllers
-app.use("/", categoriesController)
-app.use("/", articlesController)
+app.use("/", categoriesController);
+app.use("/", articlesController);
 
 app.get("/", (req, res) => {
-    Article.findAll({
-        include: [
-            {model: Category}
-        ],
-        order: [
-            ['id', 'DESC']
-        ]
-    }).then(articles => {
-        Category.findAll().then(categories => {
-            res.render('index', {articles:articles, categories:categories})
-        })
-    })
-})
+  Article.findAll({
+    include: [{ model: Category }],
+    order: [["id", "DESC"]],
+  }).then((articles) => {
+    Category.findAll().then((categories) => {
+      res.render("index", { articles: articles, categories: categories });
+    });
+  });
+});
 
 app.get("/:slug", (req, res) => {
-    var slug = req.params.slug;
+  var slug = req.params.slug;
 
-    Article.findOne({
-        where: {slug: slug}
-    }).then(article => {
-        if(article != undefined) {
-            Category.findAll().then(categories => {
-                res.render('article', {categories:categories, article:article})
-            })
-        } else {
-            res.redirect('/')
-        }
-    }).catch(err => {
-        res.redirect('/')
+  Article.findOne({
+    where: { slug: slug },
+  })
+    .then((article) => {
+      if (article != undefined) {
+        Category.findAll().then((categories) => {
+          res.render("article", { categories: categories, article: article });
+        });
+      } else {
+        res.redirect("/");
+      }
     })
-})
+    .catch((err) => {
+      res.redirect("/");
+    });
+});
+
+app.get("/category/:slug", (req, res) => {
+  var slug = req.params.slug;
+
+  Category.findOne({
+    where: { slug: slug },
+    include: [{ model: Article }],
+  })
+    .then((category) => {
+      if (category != undefined) {
+        Category.findAll().then((categories) => {
+          res.render("index", {
+            articles: category.articles,
+            categories: categories,
+          });
+        });
+      } else {
+        res.send("vish deu merda");
+      }
+    })
+    .catch((err) => {
+      console.log("deu merda");
+    });
+});
 
 //Server on
 app.listen(8080, () => {
-    console.log('Servidor ligado com sucesso.')
-})
+  console.log("Servidor ligado com sucesso.");
+});
